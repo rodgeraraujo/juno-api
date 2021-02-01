0f00103f6c9d7ca18dfab42167bc8e93a6f413ec
Rogério Araújo <rogerio.araujo@mail.com>
2021-02-01 18:50:40 -0300

    feat: added refund and capture to payment resource

--- resources/payment.js
+++ resources/payment.js
@@ -12,12 +12,35 @@ const base = require('../mixins/index');
  * @public
  */
 function Payment(juno) {
-  this.juno = juno;
+    this.juno = juno;
 
-  this.name = 'payments';
-  this.key = '';
+    this.name = 'payments';
+    this.key = '';
 }
 
 assign(Payment.prototype, base);
 
+/**
+ * Creates a total or partial refunds of credit card transaction.
+ *
+ * @param {Number} id Payment ID
+ * @return {Promise} Promise that resolves with the result
+ * @public
+ */
+Payment.prototype.refund = function refund(id) {
+    const url = this.buildUrl(`${id}/refunds`);
+    return this.juno.request(url, 'POST', undefined).then((body) => body);
+};
+
+/**
+ * Total or partial capture of previously authorized credit card transaction.
+ *
+ * @param {Number} id Payment ID
+ * @return {Promise} Promise that resolves with the result
+ * @public
+ */
+Payment.prototype.refund = function refund(id) {
+    const url = this.buildUrl(`${id}/capture`);
+    return this.juno.request(url, 'POST', undefined).then((body) => body);
+};
 module.exports = Payment;
